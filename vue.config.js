ba00eed4894f6690848b6a1fdf3b48616c507a21
Ferdinadaedth <115557428+Ferdinadaedth@users.noreply.github.com>
2023-08-28 14:04:13 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+    lintOnSave: false,
+    productionSourceMap: false,
+    outputDir:"build", //设置打包后的项目目录名称
+    // 放置静态资源的地方 (js/css/img/font/...)
+    assetsDir:'static',
+    devServer:{
+        hotOnly:true,
+    },
+    publicPath: './'
+ 
+};
